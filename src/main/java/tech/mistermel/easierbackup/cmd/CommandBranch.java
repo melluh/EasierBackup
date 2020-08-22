83b638929f7703449e6f5c9cf7dc1a6701ec2610
Melle Moerkerk <melle@mistermel.tech>
2020-08-22 16:29:55 +0200

    Fix tab completion (again)

--- src/main/java/tech/mistermel/easierbackup/cmd/CommandBranch.java
+++ src/main/java/tech/mistermel/easierbackup/cmd/CommandBranch.java
@@ -42,7 +42,7 @@ public abstract class CommandBranch extends SubCommand {
 	
 	@Override
 	public List<String> onTabComplete(CommandSender sender, String[] args) {
-		if(args.length == 0) {
+		if(args.length == 0 || args[0].isEmpty()) {
 			return new ArrayList<String>(subCommands.keySet());
 		}
 		
