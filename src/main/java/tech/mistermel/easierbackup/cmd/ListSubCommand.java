fa5c72e1787238c8b9457b58b433f71dfe871d64
MisterMelDev <mellemoerkerk@gmail.com>
2021-08-01 12:53:34 +0200

    Add extra info to /backup list

--- src/main/java/tech/mistermel/easierbackup/cmd/ListSubCommand.java
+++ src/main/java/tech/mistermel/easierbackup/cmd/ListSubCommand.java
@@ -2,6 +2,8 @@ package tech.mistermel.easierbackup.cmd;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Date;
@@ -36,8 +38,34 @@ public class ListSubCommand extends SubCommand {
 			return;
 		}
 		
+		long lastBackup = files.get(0).lastModified();
+		Duration timeAgo = Duration.between(Instant.ofEpochMilli(lastBackup), Instant.now());
 		sender.sendMessage(ChatColor.GREEN + Integer.toString(files.size()) + " " + (files.size() == 1 ? "backup" : "backups") + " made:");
+		sender.sendMessage(ChatColor.GRAY + "Your last backup was " + this.formatDuration(timeAgo) + " ago");
 		files.forEach(file -> sender.sendMessage(ChatColor.GRAY + "- " + this.formatFile(file)));
+	}
+	
+	private String formatDuration(Duration duration) {
+		if(duration.getSeconds() < 60)
+			return duration.getSeconds() + " seconds";
+		
+		long minutes = duration.toMinutes();
+		if(minutes < 60)
+			return minutes + " minutes";
+		
+		long hours = duration.toHours();
+		if(duration.toHours() < 24) {
+			long minutesPart = minutes % 60;
+			return String.format(hours + " " + plural("hour", hours) + (minutesPart != 0 ? " and " + minutesPart + " " + plural("minute", minutesPart) : ""));
+		}
+		
+		long days = duration.toDays();
+		long hoursPart = hours % 24;
+		return String.format(days + " " + plural("day", days) + (hoursPart != 0 ? " and " + hoursPart + " " + plural("hour", hoursPart) : ""));
+	}
+	
+	private String plural(String singular, long amount) {
+		return singular + (amount == 1 ? "" : "s");
 	}
 	
 	private String formatFile(File file) {
